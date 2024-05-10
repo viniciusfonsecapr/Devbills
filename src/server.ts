a2dde1b1bd0a57d8081315627bfe68745870e1c6
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-05-10 00:24:46 -0300

    routes + entity

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { json } from "express";
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(json());
+app.use(routes);
+
 app.listen(3333, () => console.log("💥 App is running at port 3333!"));
