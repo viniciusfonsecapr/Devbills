ddc5c9f398e90c470b9a84e9d59b3be89bb116f5
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-06-02 01:53:20 -0300

    Schema Validation

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -3,6 +3,8 @@ import { CategoriesServices } from "../services/categories.services";
 import { CategoriesRepository } from "../database/repositories/categories.repository";
 import { CategoryModel } from "../database/schemas/category.schema";
 import { CreateCategoryDTO } from "../dtos/categories.dto";
+import { z } from "zod";
+import { StatusCodes } from "http-status-codes";
 
 export class CategoriesController {
   async create(
@@ -18,7 +20,7 @@ export class CategoriesController {
 
       const result = await service.create({ title, color });
 
-      return res.status(201).json(result);
+      return res.status(StatusCodes.CREATED).json(result);
     } catch (err) {
       next(err);
     }
--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
+import { createCategorySchema } from "../dtos/categories.dto";
 import { CategoriesController } from "../controllers/categories.controller";
+import { ParamsType, validator } from "../middleware/validator.middleware";
 
 export const categoriesRoutes = Router();
 
 const controller = new CategoriesController();
 
-categoriesRoutes.post("/", controller.create);
+categoriesRoutes.post(
+  "/",
+  validator({
+    schema: createCategorySchema,
+    type: ParamsType.BODY,
+  }),
+  controller.create
+);
